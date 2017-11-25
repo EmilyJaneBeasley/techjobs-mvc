f99092879176abe2381baec648ee0adc84e66fef
EmilyJaneBeasley <mizem1977@yahoo.com>
2017-11-24 19:37:04 -0600

    techjobs-mvc initial commit

--- src/main/java/org/launchcode/controllers/ListController.java
+++ src/main/java/org/launchcode/controllers/ListController.java
@@ -56,10 +56,12 @@ public class ListController {
     public String listJobsByColumnAndValue(Model model,
             @RequestParam String column, @RequestParam String value) {
 
+
         ArrayList<HashMap<String, String>> jobs = JobData.findByColumnAndValue(column, value);
         model.addAttribute("title", "Jobs with " + columnChoices.get(column) + ": " + value);
         model.addAttribute("jobs", jobs);
 
         return "list-jobs";
+
     }
 }
